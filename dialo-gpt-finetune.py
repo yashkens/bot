34c5b375f514f830657a5768dfe398d854542b01
yashkens <42929295+yashkens@users.noreply.github.com>
2021-05-15 21:46:06 +0300

    Add files via upload

--- dialo-gpt-finetune.py
+++ dialo-gpt-finetune.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
 from torch.utils.data.distributed import DistributedSampler
-from tqdm.notebook import tqdm, trange
+from tqdm import tqdm, trange
 from collections import Counter
 
 from transformers import (
